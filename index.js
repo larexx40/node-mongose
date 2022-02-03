dccf62d1819fdc2d55ca318aedb6430820f96c93
larexx40 <larexx40@gmail.com>
2022-02-03 14:43:32 +0100

    connect mongoose with mongodb

---
+++ index.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+
+const Dishes = require('./models/dishes')
+
+const url = 'mongodb://localhost:27017/confusion'
+const connect = mongoose.connect(url)
+
+connect.then((db)=>{
+    console.log('Connected to server succesfully')
+
+    var newDish = Dishes({
+        name: "Efo-riro pelu ponm",
+        description: "delicious vegetable with iru "
+    })
+
+    newDish.save()
+    .then((dish)=>{
+        console.log(dish);
+
+        return Dishes.find({});
+    })
+    .then((dishes)=>{
+        console.log(dishes);
+
+        return Dishes.deleteOne({})
+    })
+    .then(()=>{
+        return mongoose.connection.close();
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
+})
